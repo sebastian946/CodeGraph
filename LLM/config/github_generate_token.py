ea027b1cd897f94c82246b14d98687a7c03a8f9e
sebastian946 <jhoan1113@hotmail.com>
2026-05-09 08:48:05 -0500

    Add config token and get token

--- LLM/config/github_generate_token.py
+++ LLM/config/github_generate_token.py
@@ -10,3 +10,10 @@ class GithubTokenManager:
         with open("codegraphtest.2026-05-08.private-key.pem", "r") as key_file:
             private_key = key_file.read()
         auth = Auth.AppAuth(app_id=self.github_app_id,private_key=private_key)
+        g = Github(auth=auth)
+        return g
+    
+    def get_repo(self, user:str, repo:str):
+        g = self.config_token()
+        repository = g.get_user(user).get_repo(repo)
+        return repository
